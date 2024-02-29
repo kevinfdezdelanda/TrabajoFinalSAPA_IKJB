ff5e0f8a75c6d6cef9ed1f6d8f0dc191085c88cf
kevinfdezdelanda <61690864+kevinfdezdelanda@users.noreply.github.com>
2024-02-29 18:30:19 +0100

    correcion errores

--- app/chatbot.py
+++ app/chatbot.py
@@ -10,10 +10,10 @@ bp = Blueprint('chatbot', __name__)
 
 @bp.route('/')
 def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('chatbot/index.html', posts=posts)
+    # db = get_db()
+    # posts = db.execute(
+    #     'SELECT p.id, title, body, created, author_id, username'
+    #     ' FROM post p JOIN user u ON p.author_id = u.id'
+    #     ' ORDER BY created DESC'
+    # ).fetchall()
+    return render_template('chatbot/index.html')
