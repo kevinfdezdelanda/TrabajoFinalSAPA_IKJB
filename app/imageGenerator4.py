e1245c0933cd6868ce081260f8c4bc9551132492
Juanan <jonrula@gmail.com>
2024-03-11 19:00:32 +0100

    imageGenerator

---
+++ app/imageGenerator4.py
@@ -0,0 +1,8 @@
+from diffusers import StableDiffusionPipeline
+import torch
+model_id = "prompthero/openjourney"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to("cpu")
+prompt = "retro serie of different cars with different colors and shapes, mdjrny-v4 style"
+image = pipe(prompt).images[0]
+image.save("./retro_cars.png")
